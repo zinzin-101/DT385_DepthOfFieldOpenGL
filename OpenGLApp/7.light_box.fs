ed69721372d4bed4e8453040d33f2a6b3503129b
zinzin-101 <96015391+zinzin-101@users.noreply.github.com>
2026-01-23 15:54:14 +0700

    Initial commit

---
+++ OpenGLApp/7.light_box.fs
@@ -0,0 +1,45 @@
+#version 330 core
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
+
+uniform vec3 lightColor;
+
+float LinearizeDepth(float depth);
+
+void main()
+{           
+    FragColor = vec4(lightColor, 1.0);
+    float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+    if(brightness > 1.0)
+        BrightColor = vec4(FragColor.rgb, 1.0);
+	else
+		BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+
+    float depth = LinearizeDepth(gl_FragCoord.z);
+
+    BrightColor = FragColor;
+
+    FragColor = vec4(vec3(0.0), 1.0);
+
+    if (depth < 0.5){
+        BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+        FragColor = vec4(lightColor, 1.0);
+    }
+
+}
+
+float LinearizeDepth(float depth)
+{
+    float far_plane = 1.0;
+    float near_plane = 0.01;
+
+    float z = depth * 2.0 - 1.0; // Back to NDC 
+    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - z * (far_plane - near_plane));	
+}
